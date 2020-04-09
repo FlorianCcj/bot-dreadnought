7d30e43a2844885bd2aaa2833ba7a2302b7fe55b
florianccj <florianccj@gmail.com>
2020-04-09 21:19:03 +0200

    [+][slug and shorcut] add easy command and slugify message

--- src/events/message.event.js
+++ src/events/message.event.js
@@ -3,26 +3,76 @@ const init = require('../commands/init.command');
 const help = require('../commands/help.command');
 const master = require('../commands/master.command');
 const set = require('../commands/set.command');
+const utils = require('../utils/utils');
 
 module.exports = async (client, msg) => {
-  console.log(msg.content);
+  let print_help = true;
+  console.log('info-' + msg.author.username + ': ' + msg.content);
+
   const { config } = client;
 
   const channel_to_post = msg.client.channels.cache.find(c => c.name === config.CHANNEL_TO_POST_NAME);
   config.is_master = msg.member.roles.cache.has(config.MASTER_GROUP_ID);
 
-  // @Todo  help
-
   // init bot
   if (msg.content.startsWith(`${config.PREFIX} init 1`)) {
     // create post if there is no bot post
     init.init1(channel_to_post, config.MESSAGE_INIT.init, config.BOT_ID);
+    print_help = false;
   } else if (msg.content.startsWith(`${config.PREFIX} init 2`)) {
     init.init2(channel_to_post, config);
+    print_help = false;
   } else if (msg.content.startsWith(`${config.PREFIX} set`)) {
     set.set(msg, config);
-  } else if (
-    msg.content.startsWith(`${config.PREFIX}`)
+    print_help = false;
+  }
+
+  // someone challenge the master
+  if (
+    utils.slug(msg.content).includes(utils.slug(config.COMMANDS_PHRASES.challenge_phrase))
+    || msg.content.startsWith(`${config.PREFIX} fight`)
+  ) {
+    challenge.launch(msg, channel_to_post, config.CHALLENGER_MESSAGE_ID, config);
+    print_help = false;
+  }
+
+  // resign the challenge
+  if (
+    utils.slug(msg.content).includes(utils.slug(config.COMMANDS_PHRASES.resign_phrase))
+    || msg.content.startsWith(`${config.PREFIX} resign`)
+  ) {
+    challenge.resign(
+      channel_to_post,
+      msg,
+      config.CHALLENGER_MESSAGE_ID,
+      msg.author.username,
+      config.RETURN_MESSAGE.not_be_challenger,
+      config.RETURN_MESSAGE.remove_challenge_list
+    );
+    print_help = false;
+  }
+
+  // master win
+  if (
+    utils.slug(msg.content).includes(utils.slug(config.COMMANDS_PHRASES.dreadnought_win_phrase))
+    || msg.content.startsWith(`${config.PREFIX} win`)
+  ) {
+    master.win(msg, channel_to_post, config);
+    print_help = false;
+  }
+
+  // master lose
+  if (
+    utils.slug(msg.content).includes(utils.slug(config.COMMANDS_PHRASES.dreadnought_lose_phrase))
+    || msg.content.startsWith(`${config.PREFIX} lose`)
+  ) {
+    master.lose(msg, channel_to_post, config);
+    print_help = false;
+  }
+
+  if (
+    print_help
+    && msg.content.startsWith(`${config.PREFIX}`)
     || msg.content.startsWith(`${config.PREFIX} -h`)
     || msg.content.startsWith(`${config.PREFIX} -help`)
     || msg.content.startsWith(`${config.PREFIX} -?`)
@@ -33,33 +83,5 @@ module.exports = async (client, msg) => {
     help.print_help(msg, config);
   }
 
-  // someone challenge the master
-  if (msg.content.toLowerCase().includes(config.COMMANDS_PHRASES.challenge_phrase.toLowerCase())) {
-    challenge.launch(msg, channel_to_post, config.CHALLENGER_MESSAGE_ID, config);
-  }
-
-  // resign the challenge
-  if (msg.content.toLowerCase().includes(config.COMMANDS_PHRASES.resign_phrase.toLowerCase())) {
-    challenge.resign(
-      channel_to_post,
-      msg,
-      config.CHALLENGER_MESSAGE_ID,
-      msg.author.username,
-      config.RETURN_MESSAGE.not_be_challenger,
-      config.RETURN_MESSAGE.remove_challenge_list
-    );
-  }
-
-  // master win
-  if (msg.content.toLowerCase().includes(config.COMMANDS_PHRASES.dreadnought_win_phrase.toLowerCase())) {
-    master.win(msg, channel_to_post, config);
-  }
-
-  // master lose
-  if (msg.content.toLowerCase().includes(config.COMMANDS_PHRASES.dreadnought_lose_phrase.toLowerCase())) {
-    master.lose(msg, channel_to_post, config);
-  }
-
-  // @Todo, Bad could not defeat Bad
   // @Todo high score during
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,7 +8,15 @@ function from_date_to_password(hash = 0) {
   return +(format_date(new Date()) + new Date().getHours()).split('/').join('') + hash;
 }
 
+function slug(msg_content) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+  return msg_content.toString().toLowerCase().replace(p, c => b.charAt(a.indexOf(c)));
+}
+
 module.exports = {
   format_date,
-  from_date_to_password
+  from_date_to_password,
+  slug
 };
